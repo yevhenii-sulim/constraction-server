813b4f80b27059155b693dd606fa6eac19c6a9e3
Yevhenii Sulim <bees127@ukr.net>
2026-04-16 16:41:29 +0300

    rename proxy

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
 import {languages, defaultLng} from './i18n/config';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
   const hasLang = languages.some((lng) => pathname.startsWith(`/${lng}`));
@@ -10,6 +10,8 @@ export function middleware(req: NextRequest) {
   if (!hasLang) {
     return NextResponse.redirect(new URL(`/${defaultLng}${pathname}`, req.url));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
